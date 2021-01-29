e8a50ce7507e0a86019f59ba9236b406899f4fe2
Luiz Felicio <unifelicio@gmail.com>
2021-01-29 10:49:39 -0300

    Add hyper dracula to config

--- .hyper.js
+++ .hyper.js
@@ -149,12 +149,12 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ['hyper-dracula],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: ['hyper-hypa-theme'],
+  localPlugins: [],
 
   keymaps: {
     'window:devtools': 'cmd+alt+j'
