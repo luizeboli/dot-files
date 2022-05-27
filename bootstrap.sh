473885cebdb770b226f0b6c8012759d86a4560d6
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-05-27 14:21:39 -0300

    Add zsh plugins installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,8 +19,19 @@ create_symlinks() {
 
 create_symlinks
 
-echo "Setting up the Spaceship theme."
-sudo apt-get install powerline fonts-powerline -y
+echo "Setting up environment vars..."
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+echo "Setting up the Spaceship theme..."
+sudo apt-get install powerline fonts-powerline -y
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+echo "Installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+echo "Installing zsh-nvm..."
+git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
+
+echo "Installing zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
